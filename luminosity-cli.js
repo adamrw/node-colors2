445e9766bdd600d773370b02db2b1dd342073c4b
Adam Weiner <adamwineguy@gmail.com>
2014-10-13 15:49:37 -0600

    finished

--- luminosity-cli.js
+++ luminosity-cli.js
@@ -1,2 +1,6 @@
 var colorutil = require('./colorutil.js');
-console.log(colorutil);
+var r = process.argv[2];
+var g = process.argv[3];
+var b = process.argv[4];
+var luminosity = colorutil.luminosity(r, g, b);
+console.log(luminosity);
